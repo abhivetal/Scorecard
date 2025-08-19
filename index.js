8f19212b5d7c095e2a346a20d3408cf817d100b6
Abhishek Vetal <abhivetal8825@gmail.com>
2025-08-19 20:55:25 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+
+let homeCount = 0;
+let homeEl = document.getElementById("home-el");
+
+function homeAdd1() {
+    homeCount += 1;
+    homeEl.textContent = homeCount;
+}
+
+function homeAdd2() {
+    homeCount += 2;
+    homeEl.textContent = homeCount;
+}
+
+function homeAdd3() {
+    homeCount += 3;
+    homeEl.textContent = homeCount;
+}
+
+let guestCount = 0;
+let guestEl = document.getElementById("guest-el");
+
+function guestAdd1() {
+    guestCount += 1;
+    guestEl.textContent = guestCount;
+}
+
+function guestAdd2() {
+    guestCount += 2;
+    guestEl.textContent = guestCount;
+}
+
+function guestAdd3() {
+    guestCount += 3;
+    guestEl.textContent = guestCount;
+}
+
